43b0f2b1d81af828a624701f95355694811d8ecb
mitselek <mihkel.putrinsh@gmail.com>
2022-02-06 07:35:50 +0200

    http retry

--- base_memo.py
+++ base_memo.py
@@ -1,6 +1,8 @@
-from email import header
 from lxml import html
+
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 import csv
 import json
@@ -9,35 +11,39 @@ import progressbar
 import os
 import sys
 
+
 print(f'Fetching records from {sys.argv[1]} to {sys.argv[2]}')
 
-# RECORD_NUMBERS = range(1, 10)
-# RECORD_NUMBERS = range(10, 100)
-# RECORD_NUMBERS = range(100, 1000)
-# rec = 785
-# RECORD_NUMBERS = range(rec, rec+1)
-# RECORD_NUMBERS = range(1000, 1100)
-# RECORD_NUMBERS = range(1100, 100000)
 MAX_RECORD = 3115101
 n_from = int(sys.argv[1])
 n_to = int(sys.argv[2])
 RECORD_NUMBERS = range(n_from, n_to)
-# RECORD_NUMBERS = range(1, MAX_RECORD + 1)
 
 fieldnames = ['ID', 'Name', 'Место проживания', 'Адрес', 
     'Год рождения', 'Место рождения', 'Реабилитирован', 'Дата реабилитации', 
     'Национальность', 'Осуждён', 'Дата осуждения',
-    'Работа', 'Расстреляна', 'Дата ареста',
+    'Работа', 'Расстреляна', 'Дата ареста', 'Образование', 'Мера пресечения', 'Партийность',
     'Умерла в местах заключения', 'Умер в местах заключения', 'Архивное дело',
     'Обвинение', 'Приговор', 'Источник']
 
-
 nr = 1
 persons = []
+
+retry_strategy = Retry(
+    total=10,
+    status_forcelist=[429, 500, 502, 503, 504],
+    method_whitelist=["HEAD", "GET", "OPTIONS"]
+)
+adapter = HTTPAdapter(max_retries=retry_strategy)
+http = requests.Session()
+http.mount("https://", adapter)
+http.mount("http://", adapter)
+
+
 for nr in progressbar.progressbar(RECORD_NUMBERS):
     url = f'https://base.memo.ru/person/show/{nr}'
-    page = requests.get(url)
-    tree = html.fromstring(page.content)
+    response = http.get(url)
+    tree = html.fromstring(response.content)
 
     # body > section > div.inner_text > div > div.cont_l.cont_l2 > div.line_t_table
     # <div class="line_t_table">Абдулахатова Кульше Исмагуловна</div>
